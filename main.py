81cff980bcde28c2cce84d141c2aa4ec126c751b
Clayton Northey <claytonnorthey@Claytons-MacBook-Pro.local>
2016-12-04 20:46:35 -0500

    getting observational probabilities

--- main.py
+++ main.py
@@ -3,11 +3,36 @@ import numpy
 from python_speech_features import mfcc
 from os import listdir, remove
 from matplotlib import pyplot as plt
+import random
 import scipy.io.wavfile as wav
 import speech_recognition
 
 dictionary = {}
 single_letter_mapping = {}
+transitional_numbers = {}
+valid_test_words = []
+
+extra_dictionary_lines = [
+	'C C',
+	'C CH',
+	'Q QUA',
+	'E EH1',
+	'A EH1',
+	'S Z',
+	'IE IY1',
+	'T DT',
+	'ER ER0'
+]
+
+def get_observational_proabability(value, time, dictionary):
+	total_states = 0.0
+	found_states = 0.0
+	for word in dictionary:
+		word_length = len(word)
+		if time < word_length and word[time:time+len(word)] == value:
+			found_states += 1.0
+		total_states += 1.0
+	return found_states/total_states
 
 if __name__=='__main__':
 
@@ -15,16 +40,44 @@ if __name__=='__main__':
 	f = open('dictionary.txt')
 	data = f.read().split('\n')
 	f.close()
+	data.extend(extra_dictionary_lines)
 	for line in data:
 		data_items = line.split()
 		dictionary[data_items[0]] = data_items[1:]
-		if data_items[0].isalpha() and len(data_items[0]) == 1:
-			for phone in dictionary[data_items[0]]:
+		for index, phone in enumerate(dictionary[data_items[0]]):
+			if len(data_items[1:]) == 1 or (data_items[0].isalpha() and len(data_items[0]) < 3):
 				if phone not in single_letter_mapping:
 					single_letter_mapping[phone] = []
 				single_letter_mapping[phone].append(data_items[0])
-	print single_letter_mapping
-	# STEP 1: FEATURE ANALYSIS
+			if index != len(dictionary[data_items[0]]) - 1:
+				key = '{}-{}'.format(phone, dictionary[data_items[0]][index + 1])
+				if key not in transitional_numbers:
+					transitional_numbers[key] = 0
+				transitional_numbers[key]+= 1;
 	
-	
+	valid_phones = [phone for phone in single_letter_mapping]
+	for word in dictionary:
+		valid_word = 1
+		for phone in dictionary[word]:
+			if phone not in valid_phones:
+				valid_word = 0
+		if valid_word:
+			valid_test_words.append(word)
 
+	# GET RANDOM TEST PHONEMES
+	test_word = valid_test_words[random.randint(0, len(valid_test_words))]
+	test_phones = dictionary[test_word]
+
+	print 'The test word is: ', test_word, " --- THE PROGRAM DOES NOT KNOW THIS!"
+	print 'the phonemes are: ', test_phones, " --- THE PROGRAM DOES KNOW THIS!"
+
+	output_results_map = {}
+	for time_index, phone in enumerate(test_phones):
+		possible_letters = single_letter_mapping[phone]
+		print phone, possible_letters
+		output_results_map[time_index] = {}
+		for letter in possible_letters:
+			probability = 1
+			probability *= get_observational_proabability(letter, time_index, dictionary)
+			output_results_map[time_index][letter] = probability
+			
